997637bc8d9a203800fb63846cb2a9269e1cf89e
WeiXiao <46071660+WeiXiaoSummer@users.noreply.github.com>
2019-09-27 17:27:38 +0200

    Update graf.cc

--- graf.cc
+++ graf.cc
@@ -2,7 +2,7 @@
 #include <vector>
 #include "random_graphs.hh"
 using namespace std;
-using graph = std::vector < std::vector <int> >;
+using graph = std::vector <std::vector <int> >;
 
 
 void depth_first_search(int i, std::vector<bool>& visited, std::vector<int>& partial_res, const graph& g){
@@ -32,23 +32,55 @@ void connex_components(const graph& g, std::vector < std::vector <int> >& res){
 	return;
 }
 
-int main() {
-	int numVert, p;
+void GNP_test () {
+	int numVert;
+	double p;
 	bool directed;
 	cin >> numVert >> p >> directed;
 	graph g_test;
 	g_test = erdos_renyi_random_graph(numVert, p, directed);
-	vector<int> partial;
-	vector<bool> visited (10, false);
-	depth_first_search(0, visited, partial, g_test);
-	for (int i : partial) {
-		cout << i << " ";
+	cout << "\n\nAdjacency Matrix\n\n";
+	for (vector<int> v : g_test) {
+		for (int i : v) {
+			cout << i << " ";
+		}
+		cout << endl;
 	}
-	cout << endl;
+	cout << "\n\nConnected Component\n\n";
 	graph component;
 	connex_components(g_test, component);
 	for (vector<int> v : component) {
 		for (int i : v) cout << i << " ";
 		cout << endl;
+	}
+	cout << endl;
+}
+
+void RGG_test () {
+	int numVert;
+	double radius;
+	cin >> numVert >> radius;
+	graph g_test;
+	g_test = random_geometric_graph(numVert, radius);
+	cout << "\n\nAdjacency Matrix\n\n";
+	for (vector<int> v : g_test) {
+		for (int i : v) {
+			cout << i << " ";
+		}
+		cout << endl;
+	}
+	cout << "\n\nConnected Component\n\n";
+	graph component;
+	connex_components(g_test, component);
+	for (vector<int> v : component) {
+		for (int i : v) cout << i << " ";
+		cout << endl;
+	}
+	cout << endl;
+}
+
+int main () {
+	while (true) {
+		GNP_test();
 	}
 }
