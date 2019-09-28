20fbb2cf088e6ef05800185463bed839887cb389
WeiXiao <46071660+WeiXiaoSummer@users.noreply.github.com>
2019-09-28 18:01:02 +0200

    Update with Edge test

--- random_graphs.hh
+++ random_graphs.hh
@@ -6,8 +6,8 @@ using graph = vector < vector <int> >;
 
 graph complete_graph(int n);
 
-graph erdos_renyi_random_graph(int n, double p, bool directed = false);
+graph erdos_renyi_random_graph(int n, double p, bool directed = false, bool edge_fixed = false, int Edges = 0);
 
-void slow_edges(graph& G,const map <int, vector<double>>& pos, double radius, double p, int dim);
+void slow_edges(graph& G,const map <int, vector<double>>& pos, double radius, double p, int dim, bool edge_fixed = false, int Edges = 0);
 
-graph random_geometric_graph(int n, double radius, int dim = 2, double p = 2.0);
+graph random_geometric_graph(int n, double radius, int dim = 2, double p = 2.0, bool edge_fixed = false, int Edges = 0);
