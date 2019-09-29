f83d541abfdb723ef4153b000f1bf9302ca14a5f
root <root@JingLuis.localdomain>
2019-09-29 20:54:26 +0200

    edge

--- edge_graphics.R
+++ edge_graphics.R
@@ -8,15 +8,15 @@ list_of_files <- list.files(path = "./", recursive = TRUE, patter = "^10 .*\\.tx
 colo = vector()
 i = 0
 leg = vector()
-inici = c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1)
-for (val in 11:1){
+inici = c(0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1)
+for (val in 1:11){
   result = paste("./", list_of_files[val], sep = "")
   leg = c(leg, paste("P = ", paste0(inici[i+1]),sep = "")) 
   dades <- read.table(result)
   value = floor(runif(1, min=1, max=657))
   colo = c(colo, colors()[value])
   if(i == 0) plot(main = "N = 10",dades$V1,dades$V2,type = "l",lty = 2, ylim = c(0,1.0), lwd = 2,col = colors()[value],ylab = "Probability that network is connected", xlab = "Number of edges")
-  else lines(dades$V1,dades$V2, col = colors()[floor(runif(1, min=1, max=657))], lwd = 2, type = "l", lty = 6) 
+  else lines(dades$V1,dades$V2, col = colors()[value], lwd = 2, type = "l", lty = 6) 
   i = i+1
 }
 
@@ -30,15 +30,15 @@ list_of_files <- list.files(path = "./", recursive = TRUE, patter = "^50 .*\\.tx
 colo = vector()
 i = 0
 leg = vector()
-inici = c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1)
-for (val in 11:1){
+inici = c(0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1)
+for (val in 1:11){
   result = paste("./", list_of_files[val], sep = "")
   leg = c(leg, paste("P = ", paste0(inici[i+1]),sep = "")) 
   dades <- read.table(result)
   value = floor(runif(1, min=1, max=657))
   colo = c(colo, colors()[value])
   if(i == 0) plot(main = "N = 50",dades$V1,dades$V2,type = "l",lty = 2,ylim = c(0,1.0), lwd = 2,col = colors()[value],ylab = "Probability that network is connected", xlab = "Number of edges")
-  else lines(dades$V1,dades$V2, col = colors()[floor(runif(1, min=1, max=657))], lwd = 2, type = "l", lty = 6) 
+  else lines(dades$V1,dades$V2, col = colors()[value], lwd = 2, type = "l", lty = 6) 
   i = i+1
 }
 
@@ -52,16 +52,16 @@ list_of_files <- list.files(path = "./", recursive = TRUE, patter = "^100 .*\\.t
 colo = vector()
 i = 0
 leg = vector()
-inici = c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1)
-for (val in 11:1){
+inici = c(0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1)
+for (val in 1:11){
   result = paste("./", list_of_files[val], sep = "")
   leg = c(leg, paste("P = ", paste0(inici[i+1]),sep = "")) 
   dades <- read.table(result)
   value = floor(runif(1, min=1, max=657))
   colo = c(colo, colors()[value])
-  if(i == 0) plot(main = "N = 100",dades$V1,dades$V2,type = "l",lty = 2, ylim = c(0,1.0),lwd = 2,col = colors()[value],ylab = "Probability that network is connected", xlab = "Number of edges")
+  if(i == 0) plot(main = "N = 100",dades$V1,dades$V2,type = "l",lty = 1, ylim = c(0,1.0),lwd = 2,col = colors()[value],ylab = "Probability that network is connected", xlab = "Number of edges")
   
-  else lines(dades$V1,dades$V2, col = colors()[floor(runif(1, min=1, max=657))], lwd = 2, type = "l", lty = 6) 
+  else lines(dades$V1,dades$V2, col = colors()[value], lwd = 1, type = "l", lty = 6) 
   i = i+1
 }
 
@@ -74,8 +74,8 @@ list_of_files <- list.files(path = "./", recursive = TRUE, patter = "^250 .*\\.t
 colo = vector()
 i = 0
 leg = vector()
-inici = c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1)
-for (val in 11:1){
+inici = c(0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1)
+for (val in 1:11){
   result = paste("./", list_of_files[val], sep = "")
   leg = c(leg, paste("P = ", paste0(inici[i+1]),sep = "")) 
   dades <- read.table(result)
@@ -83,7 +83,7 @@ for (val in 11:1){
   colo = c(colo, colors()[value])
   if(i == 0) plot(main = "N = 250",dades$V1,dades$V2,type = "l",lty = 2, ylim = c(0,1.0),lwd = 2,col = colors()[value],ylab = "Probability that network is connected", xlab = "Number of edges")
   
-  else lines(dades$V1,dades$V2, col = colors()[floor(runif(1, min=1, max=657))], lwd = 2, type = "l", lty = 6) 
+  else lines(dades$V1,dades$V2, col = colors()[value], lwd = 2, type = "l", lty = 6) 
   i = i+1
 }
 
@@ -95,8 +95,8 @@ list_of_files <- list.files(path = "./", recursive = TRUE, patter = "^500 .*\\.t
 colo = vector()
 i = 0
 leg = vector()
-inici = c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1)
-for (val in 11:1){
+inici = c(0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1)
+for (val in 1:11){
   result = paste("./", list_of_files[val], sep = "")
   leg = c(leg, paste("P = ", paste0(inici[i+1]),sep = "")) 
   dades <- read.table(result)
@@ -104,7 +104,7 @@ for (val in 11:1){
   colo = c(colo, colors()[value])
   if(i == 0) plot(main = "N = 500",dades$V1,dades$V2,type = "l",lty = 2, ylim = c(0,1.0),lwd = 2,col = colors()[value],ylab = "Probability that network is connected", xlab = "Number of edges")
   
-  else lines(dades$V1,dades$V2, col = colors()[floor(runif(1, min=1, max=657))], lwd = 2, type = "l", lty = 6) 
+  else lines(dades$V1,dades$V2, col = colors()[value], lwd = 2, type = "l", lty = 6) 
   i = i+1
 }
 
@@ -117,8 +117,8 @@ list_of_files <- list.files(path = "./", recursive = TRUE, patter = "^1000 .*\\.
 colo = vector()
 i = 0
 leg = vector()
-inici = c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1)
-for (val in 11:1){
+inici = c(0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1)
+for (val in 1:11){
   result = paste("./", list_of_files[val], sep = "")
   leg = c(leg, paste("P = ", paste0(inici[i+1]),sep = "")) 
   dades <- read.table(result)
@@ -126,7 +126,7 @@ for (val in 11:1){
   colo = c(colo, colors()[value])
   if(i == 0) plot(main = "N = 1000",dades$V1,dades$V2,type = "l",lty = 2, ylim = c(0,1.0),lwd = 2,col = colors()[value],ylab = "Probability that network is connected", xlab = "Number of edges")
   
-  else lines(dades$V1,dades$V2, col = colors()[floor(runif(1, min=1, max=657))], lwd = 2, type = "l", lty = 6) 
+  else lines(dades$V1,dades$V2, col = colors()[value], lwd = 2, type = "l", lty = 6) 
   i = i+1
 }
 
