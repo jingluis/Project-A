fe6a6de33b2a88cac92927cedec1bddb51cf7cc2
WeiXiao <46071660+WeiXiaoSummer@users.noreply.github.com>
2019-10-20 22:40:13 +0200

    Update random_graphs.hh

--- random_graphs.hh
+++ random_graphs.hh
@@ -10,4 +10,4 @@ graph erdos_renyi_random_graph(int n, double p, graph& adjacency, bool edge_fixe
 
 void slow_edges(graph& G,const map <int, vector<double>>& pos, double radius, double p, int dim, bool edge_fixed = false, int Edges = 0);
 
-graph random_geometric_graph(int n, double radius, int dim = 2, double p = 2.0, bool edge_fixed = false, int Edges = 0);
+graph random_geometric_graph(int n, double radius, graph& adjacency, int dim = 2, double p = 2.0, bool edge_fixed = false, int Edges = 0);
